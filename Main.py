dbd31c0d89e79a0e1227658ef840912c14f566b7
stormjoannes <stormjoannes10@hotmail.com>
2020-06-08 19:31:32 +0200

    database uitgebreid met meer informatie per kledingstuk

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
-from WeerAPI import setValues
+from WeerAPI import *
+from MatchingAppend import *
 
-print(setValues())
+def opties():
+    keuze = input("Wil je een kledingstuk toevoegen, verwijderen of uitkiezen: ").lower()
+    if keuze == 'toevoegen':
+        addClothes()
+
+    elif keuze == 'verwijderen':
+        deleteClothes()
+
+    elif keuze == 'uitkiezen':
+        pickClothes()
+
+    else:
+        print("optie is geen geldige optie")
+        opties()
+
+
+opties()
+# print(setValues())
--- MatchingAppend.py
+++ MatchingAppend.py
@@ -9,21 +9,36 @@ def addClothes():
         longShort = input('Lengte van je broeks pijpen of mouwen: lang of kort: ').lower()
         category = input('Category van je kledingstuk: Sport, Feestje of dagelijks leven: ').lower()
         color = input('Kleur van je kledingstuk: ').lower()
+        brand = input('Merk van je kledingstuk: ').lower()
+        singleCarry = input('Draag je dit kledingstuk alleen: ').lower()
 
         nieuweData = {"naam": name,
                       "langKort": longShort,
                       "gelegenheid": category,
-                      "kleur": color}
+                      "kleur": color,
+                      "merk": brand,
+                      "alleenDrager": singleCarry}
 
         data['kleding'].append(nieuweData)
-        allKleding.write(data)
-        allKleding.close()
+        json.dump(data, allKleding)
+
+def deleteClothes():
+    with open('Kledingkast.json', 'w') as allKleding:
+        name = input('Naam van je kledingstuk: ').lower()
+        longShort = input('Lengte van je broeks pijpen of mouwen: lang of kort: ').lower()
+        category = input('Category van je kledingstuk: Sport, Feestje of dagelijks leven: ').lower()
+        color = input('Kleur van je kledingstuk: ').lower()
+        brand = input('Merk van je kledingstuk: ').lower()
+        singleCarry = input('Draag je dit kledingstuk alleen: ').lower()
+
+
+        for i in data['kleding']:
+            if i['naam'] == name and i['langKort'] == longShort and i['gelegenheid'] == category and i['kleur'] == color and i['merk'] == brand and i['alleenDrager'] == singleCarry:
+                data['kleding'].remove(i)
+        json.dump(data, allKleding)
 
 
 
 def pickClothes():
     for i in data['kleding']:
         print(i)
-
-addClothes()
-# pickClothes()
