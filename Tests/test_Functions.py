b05a978631453e688f6800a5bf0bd88f050bf31d
stormjoannes <stormjoannes10@hotmail.com>
2020-06-22 11:41:37 +0200

    tests bijna af

--- Tests/test_Functions.py
+++ Tests/test_Functions.py
@@ -57,7 +57,33 @@ def test_backup_dump():
 
 
 def test_refresh_gedragen():
-    pass
+    username = "TestRefreshGedragen"
+    stad = "utrecht"
+    land = "nl"
+
+    configSignUp(username, stad, land)
+
+    gedragenSetje = [["h&m shirt wit", "wit", "shirt", "h&m", "kort"],
+     ["grijze jeans", "grijze", "jeans", "denham", "lang"], "2020-06-10"]
+
+    with open('../jsonFiles/BackupKledingkast.json', 'r') as doc:
+        allInf = json.load(doc)
+
+    with open('../jsonFiles/BackupKledingkast.json', 'w') as docWrite:
+        allInf[username][1]["gedragen"].append(gedragenSetje)
+        json.dump(allInf, docWrite)
+
+    refreshGedragen(username)
+
+    with open('../jsonFiles/BackupKledingkast.json', 'r') as doc:
+        allInf = json.load(doc)
+
+        lengteGedragenList = allInf[username][1]["gedragen"]
+
+    deleteAccount(username)
+
+    assert len(lengteGedragenList) == 0
+
 
 
 def test_geg_wijzigen():
@@ -101,9 +127,6 @@ def test_geg_wijzigen():
     deleteAccount(WijzigUserName)
 
     assert TestBeforeName == True and TestBeforeWijzigName == False and TestAfterName == False and TestAfterWijzigName == True
-
-
-
 
 
 def test_delete_account():
--- Tests/testjes.py
+++ Tests/testjes.py
@@ -1,7 +1,27 @@
 import json
+from Code.Functions import *
 
-with open('../jsonFiles/Kledingkast.json', 'r') as doc:
+username = "TestRefreshGedragen"
+stad = "utrecht"
+land = "nl"
+
+configSignUp(username, stad, land)
+
+gedragenSetje = [["h&m shirt wit", "wit", "shirt", "h&m", "kort"],
+                 ["grijze jeans", "grijze", "jeans", "denham", "lang"], "2020-06-10"]
+
+with open('../jsonFiles/BackupKledingkast.json', 'r') as doc:
     allInf = json.load(doc)
 
-for i in allInf:
-    print(i)
+with open('../jsonFiles/BackupKledingkast.json', 'w') as docWrite:
+    allInf[username][1]["gedragen"].append(gedragenSetje)
+    json.dump(allInf, docWrite)
+
+refreshGedragen(username)
+
+with open('../jsonFiles/BackupKledingkast.json', 'r') as doc:
+    allInf = json.load(doc)
+
+    lengteGedragenList = allInf[username][1]["gedragen"]
+
+deleteAccount(username)
