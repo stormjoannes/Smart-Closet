3359b93ed28a8f5179493246a3a99525f568fb5d
stormjoannes <stormjoannes10@hotmail.com>
2020-06-24 23:19:19 +0200

    aanpassinkjes

--- Code/interface.py
+++ Code/interface.py
@@ -62,7 +62,7 @@ def showMenuLoginSignup(root):
     """Dit is een aparte functie om het dropdown menu voor de signup en de login te maken"""
     # root.geometry('1920x1080')
     root.configure(background='gray')
-    root.iconphoto(False, PhotoImage(file="../pictures/kledingkastIcon.png"))
+    root.iconphoto(False, PhotoImage(file="../overige_bestanden/kledingkastIcon.png"))
 
     menu = Menu(root)
     root.config(menu=menu)
@@ -416,7 +416,7 @@ def showMenu(root):
 
     # root.geometry('1920x1080')
     root.configure(background='gray')
-    root.iconphoto(False, PhotoImage(file="../pictures/kledingkastIcon.png"))
+    root.iconphoto(False, PhotoImage(file="../overige_bestanden/kledingkastIcon.png"))
 
     toDestroyRoot = root
     menu = Menu(root)
